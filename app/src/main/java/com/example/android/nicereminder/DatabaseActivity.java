130bc8cad599d9448c50150aadeaead736cba589
Willy Tu <tu.willy.97@gmail.com>
2019-04-27 15:34:47 -0700

    Tested Firebase

--- app/src/main/java/com/example/android/nicereminder/DatabaseActivity.java
+++ app/src/main/java/com/example/android/nicereminder/DatabaseActivity.java
@@ -1,21 +1,35 @@
 package com.example.android.nicereminder;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class DatabaseActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private TextView email;
-    private TextView password;
+    private TextView emailText;
+    private TextView passwordText;
     private Button signin;
     private Button signup;
+    private Button logout;
 
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
+    private final static String TAG = "MAIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +37,205 @@ public class DatabaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_database);
 
         mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
 
-        email = (TextView)findViewById(R.id.email);
-        password = (TextView)findViewById(R.id.password);
+        emailText = (TextView)findViewById(R.id.editEmail);
+        passwordText = (TextView)findViewById(R.id.editPassword);
 
         signin = (Button) findViewById(R.id.signin);
         signup = (Button) findViewById(R.id.signup);
+        logout = (Button) findViewById(R.id.logout);
 
 
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null){
+                    finish();
+                }
+            }
+        };
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = emailText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                if(!email.isEmpty() && !password.isEmpty()){
+
+                    if(password.length() < 6){
+                        Toast.makeText(DatabaseActivity.this, "Password must have more than 6 characters!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(DatabaseActivity.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                                        // If sign in fails, display a message to the user. If sign in succeeds
+                                        // the auth state listener will be notified and logic to handle the
+                                        // signed in user can be handled in the listener.
+                                        if (!task.isSuccessful()) {
+                                            Toast.makeText(DatabaseActivity.this, "Authentication Failed",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    }
+
+                }
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailText.getText().toString();
+                String password = passwordText.getText().toString();
+                if(!email.isEmpty() && !password.isEmpty()) {
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(DatabaseActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                                    // If sign in fails, display a message to the user. If sign in succeeds
+                                    // the auth state listener will be notified and logic to handle the
+                                    // signed in user can be handled in the listener.
+                                    if (!task.isSuccessful()) {
+                                        Log.w(TAG, "signInWithEmail:failed", task.getException());
+                                        Toast.makeText(DatabaseActivity.this, "Authentication Failed",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(FirebaseAuth.getInstance() != null) {
+                    FirebaseAuth.getInstance().signOut();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 
+
 }
+
+//public class DatabaseActivity extends AppCompatActivity {
+//    private FirebaseAuth mAuth;
+//    private FirebaseAuth.AuthStateListener mAuthListener;
+//    private final static String TAG = "MAIN";
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_database);
+//
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        FirebaseUser user = mAuth.getCurrentUser();
+//
+//        mAuthListener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser user = firebaseAuth.getCurrentUser();
+//                if (user != null) {
+//                    // User is signed in
+//                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+//                } else {
+//                    // User is signed out
+//                    Log.d(TAG, "onAuthStateChanged:signed_out");
+//                }
+//            }
+//        };
+//
+//        Button signupButton = (Button) findViewById(R.id.signup);
+//        signupButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String email = ((TextView) findViewById(R.id.email)).getText().toString();
+//                String password = ((TextView) findViewById(R.id.password)).getText().toString();
+//
+//                mAuth.createUserWithEmailAndPassword(email, password)
+//                        .addOnCompleteListener(DatabaseActivity.this, new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+//
+//                                // If sign in fails, display a message to the user. If sign in succeeds
+//                                // the auth state listener will be notified and logic to handle the
+//                                // signed in user can be handled in the listener.
+//                                if (!task.isSuccessful()) {
+//                                    Toast.makeText(DatabaseActivity.this, "Authentication Failed",
+//                                            Toast.LENGTH_SHORT).show();
+//                                }
+//                            }
+//                        });
+//            }
+//        });
+//
+//        Button loginButton = (Button) findViewById(R.id.signin);
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String email = ((TextView) findViewById(R.id.email)).getText().toString();
+//                String password = ((TextView) findViewById(R.id.password)).getText().toString();
+//                mAuth.signInWithEmailAndPassword(email, password)
+//                        .addOnCompleteListener(DatabaseActivity.this, new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+//
+//                                // If sign in fails, display a message to the user. If sign in succeeds
+//                                // the auth state listener will be notified and logic to handle the
+//                                // signed in user can be handled in the listener.
+//                                if (!task.isSuccessful()) {
+//                                    Log.w(TAG, "signInWithEmail:failed", task.getException());
+//                                    Toast.makeText(DatabaseActivity.this, "Authentication Failed",
+//                                            Toast.LENGTH_SHORT).show();
+//                                }
+//                            }
+//                        });
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        mAuth.addAuthStateListener(mAuthListener);
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        if (mAuthListener != null) {
+//            mAuth.removeAuthStateListener(mAuthListener);
+//        }
+//    }
+//}
--- app/src/main/java/com/example/android/nicereminder/MainActivity.java
+++ app/src/main/java/com/example/android/nicereminder/MainActivity.java
@@ -13,18 +13,27 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private Context context;
     private Activity activity = this;
     private Button update;
-    private Button login;
+    private Button upload;
+    private Button download;
+    private TextView cloudText;
     private TextView text;
     private LocationManager locationManager;
     private LocationListener locationListener;
@@ -35,13 +44,24 @@ public class MainActivity extends AppCompatActivity {
     public static final String LOCATION = "location";
     public static final String LOCATION_ON = "location_on";
 
+    private String currentLocation;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        currentLocation = "";
+
         text = (TextView) findViewById(R.id.text);
+        cloudText = (TextView) findViewById(R.id.cloud);
         update = (Button) findViewById(R.id.button);
+        upload = (Button)findViewById(R.id.upload);
+        download = (Button)findViewById(R.id.download);
+
         context = getApplicationContext();
 
 
@@ -60,7 +80,8 @@ public class MainActivity extends AppCompatActivity {
         // Define a listener that responds to location updates
         locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
-                text.setText(location.getLatitude() + " : " + location.getLongitude());
+                currentLocation = location.getLatitude() + " : " + location.getLongitude();
+                text.setText(currentLocation);
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -88,15 +109,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        login = (Button)findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+
+        upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, DatabaseActivity.class));
+                myRef.setValue(currentLocation);
+            }
+        });
+
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
             }
         });
 
         loadData();
+
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user == null){
+            startActivity(new Intent(MainActivity.this, DatabaseActivity.class));
+        }else{
+            // User is signed in
+            if(user.getEmail().isEmpty()){
+                startActivity(new Intent(MainActivity.this, DatabaseActivity.class));
+            }
+        }
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Test");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                cloudText.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
     }
 
 
@@ -109,6 +169,8 @@ public class MainActivity extends AppCompatActivity {
         editor.putBoolean(LOCATION_ON, SearchLocation);
 
         editor.commit();
+
+        mAuth.signOut();
 
         super.onDestroy();
     }
